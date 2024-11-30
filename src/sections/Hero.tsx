c36b840561a4da2eb042bd623fe3f4fcc8a77ee0
Abdo-max34 <Abdo-max34>
2024-11-30 22:38:09 +0100

    Auto-update: Sat 11/30/2024 22:38:09.60

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,17 +1,30 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
 import Image from "next/image";
 export const HeroSection = () => {
-  return <div>
+  return <div className="py-32">
     <div className="container">
-    <Image src={memojiImage} alt="Person"  />
-        <div>
-            <div></div>
-            <div>Available for new project</div>
+        <div className="flex flex-col items-center">
+    <Image src={memojiImage} className="size-[100px]" alt="Person"  />
+        <div className="bg-gray-900 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">Available for new project</div>
         </div>
-        <h1>Building Exceptional User Experiences</h1>
+        </div>
+        <h1 className="font-serif text-3xl text-center mt-8">Building Exceptional User Experiences</h1>
         <p>I specialize in transforming designs into functional,
             high-perfoming web applications. Let&apos;s discuss your next project.
         </p>
+        <div>
+            <button>
+                <span>Explore My Work</span>
+                <ArrowDown/>
+            </button>
+            <button>
+                <span>👋</span>
+                <span>Lest&apos;s Connect</span>
+            </button>
+        </div>
     </div>
   </div>;
 };
