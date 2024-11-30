5195db0c237e478a350b9bb79b06ea0b4306ce88
Abdo-max34 <Abdo-max34>
2024-11-30 22:17:02 +0100

    Auto-update: Sat 11/30/2024 22:17:02.83

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,15 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return <div>
+    <div className="container">
+    <Image src={memojiImage} alt="Person"  />
+        <div>
+            <div></div>
+            <div>Available for new project</div>
+        </div>
+        <h1>Building Exceptional User Experiences</h1>
+        <p>I'm a skilled front-end developer with a passion for crafting intuitive and visually stunning digital products.</p>
+    </div>
+  </div>;
 };
