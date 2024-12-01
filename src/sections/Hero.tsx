633ac438648c564603f84d0bfacd6e9ead76df77
Abdo-max34 <Abdo-max34>
2024-12-01 09:22:10 +0100

    Auto-update: Sun 12/01/2024  9:22:10.88

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,12 +3,14 @@ import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import Image from "next/image";
 export const HeroSection = () => {
-  return( <div className="py-32 md:py-40 lg:py-42 relative z-0">
+  return( <div className="py-32 md:py-48 lg:py-60 relative z-0">
     <div className="absolute inset-0 -z-30 opacity-5" style={{
         backgroundImage:`url(${grainImage.src})`
     }}></div>
     <div className="size-[620px] hero-ring"></div>
     <div className="size-[820px] hero-ring"></div>
+    <div className="size-[1020px] hero-ring"></div>
+    <div className="size-[1220px] hero-ring"></div>
     <div className="container">
         <div className="flex flex-col items-center">
     <Image src={memojiImage} className="size-[100px]" alt="Person"  />
