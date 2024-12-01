3646e057a323ef082b0a7d9ade0be632d7f9d2fb
Abdo-max34 <Abdo-max34>
2024-12-01 08:56:58 +0100

    Auto-update: Sun 12/01/2024  8:56:58.70

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,7 +13,7 @@ export const HeroSection = () => {
         </div>
         <div className="max-w-lg mx-auto">
         <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
-        <p className="mt-4 text-center text-white/60">I specialize in transforming designs into functional,
+        <p className="mt-4 text-center text-white/60 md:text-lg">I specialize in transforming designs into functional,
             high-perfoming web applications. Let&apos;s discuss your next project.
         </p>
         </div>
