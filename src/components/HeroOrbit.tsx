b39a03bf7aa725fd193d8ab39ee151dd4f9e012e
Abdo-max34 <Abdo-max34>
2024-12-01 11:08:31 +0100

    Auto-update: Sun 12/01/2024 11:08:31.35

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,10 +1,10 @@
 import { PropsWithChildren } from "react";
 
-export const HeroOrbit = ({children,size}:PropsWithChildren<{size:number}>) => {
+export const HeroOrbit = ({children,size,rotation}:PropsWithChildren<{size:number;rotation:number}>) => {
     return (
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="border border-red-500"style={{height:`${size}px`,width:`${size}px`}}>
-            <div className="border border-r-red-500 inline-flex">{children}</div>
+        <div className=""style={{transform:`rotate(${rotation}deg)`,height:`${size}px`,width:`${size}px`}}>
+            <div className="inline-flex" style={{transform:`rotate(${rotation * -1}deg)`}}>{children}</div>
         </div>
         </div>
     );
